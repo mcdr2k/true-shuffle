46d4f2c150139fe819e3774bae65731ef88c421c
Mart de Roos <m.c.deroos@student.utwente.nl>
2024-10-23 20:56:06 +0200

    Document paging package

--- src/main/java/nl/martderoos/trueshuffle/paging/PageAggregator.java
+++ src/main/java/nl/martderoos/trueshuffle/paging/PageAggregator.java
@@ -6,13 +6,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Utility class for aggregating pages of data into a single list
+ */
 public class PageAggregator {
     private PageAggregator() {
 
     }
 
-    public static <T> List<T> aggregate(SpotifyFuturePage<T> target, int hardLimit) throws FatalRequestResponse {
-        var page = target.execute();
+    /**
+     * Aggregates the provided initial page's items and all subsequent pages' items into a single list of items.
+     *
+     * @param initialPage the first page from which we will start aggregating.
+     * @param hardLimit   the limit on the total number of items this function may return at most.
+     * @param <T>         the type of items we may return
+     * @return the list of items, never null but may be empty.
+     * @throws FatalRequestResponse when a page fails to load.
+     */
+    public static <T> List<T> aggregate(SpotifyFuturePage<T> initialPage, int hardLimit) throws FatalRequestResponse {
+        var page = initialPage.load();
         hardLimit = Math.min(page.getTotal(), hardLimit);
 
         List<T> result = new ArrayList<>(hardLimit);
@@ -20,13 +32,14 @@ public class PageAggregator {
 
         var next = page.getNext();
         while (next != null && result.size() < hardLimit) {
-            page = next.execute();
+            page = next.load();
             addSome(result, Arrays.asList(page.getItems()), hardLimit - result.size());
             next = page.getNext();
         }
 
         return result;
     }
+
 
     private static <T> void addSome(List<T> target, List<T> items, int count) {
         target.addAll(items.subList(0, Math.min(items.size(), count)));
